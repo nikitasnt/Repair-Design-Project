77cbc89a341e6dd24e4b21ad6e2e6b0932b179e6
Nikita <14nikita14m@gmail.com>
2020-03-19 00:15:06 +0400

    feat: add fonts Yeseva One & FuturaBook & FuturaDemi and edit gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -12,15 +12,18 @@ function bs() {
   });
   watch("./*.html").on('change', browserSync.reload);
   watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
   watch("./js/*.js").on('change', browserSync.reload);
 };
 
 // sass
 function serveSass() {
-  return src("./sass/*.sass")
+  return src("./sass/**/*.sass", "./sass/**/*.scss")
     .pipe(sass())
     .pipe(dest("./css"))
     .pipe(browserSync.stream());
 };
 
 exports.serve = bs;
+
+// "gulp serve"
