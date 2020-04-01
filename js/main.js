8a950ae8123665415f91ad1408f5d2129f2544de
Nikita <14nikita14m@gmail.com>
2020-04-01 17:49:36 +0400

    refact: on jQuery JS code and add up scroll button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function (event) {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
@@ -12,6 +13,35 @@ document.addEventListener("DOMContentLoaded", function (event) {
     element.addEventListener('click', switchModal);
   });
   closeBtn.addEventListener('click', switchModal);
+});
+*/
+
+$(document).ready(function () {
+  // закртыие и открытие модального окна кликами мыши
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
   
-  
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal_visible');
+  });
+  closeBtn.on('click', function () {
+    modal.removeClass('modal_visible');
+  });
+
+  // кнопка наверх
+  var button = $('.up-button');	
+  $(window).scroll (function () {
+    if ($(this).scrollTop () > 300) {
+      button.fadeIn();
+    } else {
+      button.fadeOut();
+    }
+  });	 
+  button.on('click', function(){
+  $('body, html').animate({
+  scrollTop: 0
+  }, 800);
+  return false;
+  });	
 });
