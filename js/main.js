6b4dda7e2338227ea7adb2a1685745c230cea95e
Nikita <14nikita14m@gmail.com>
2020-04-03 00:12:46 +0400

    feat: add projects section

--- js/main.js
+++ js/main.js
@@ -44,5 +44,25 @@ $(document).ready(function () {
       scrollTop: 0
     }, 800);
     return false;
-  });	
+  });
+
+  // Swiper Slider для секции "Завершенные Проекты"
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 25 + bullets.width() + 13)
+  bullets.css('left', prev.width() + 25)
 });
